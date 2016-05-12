3080ca81e82f75ab26e9f71110f6aba3faa40b40
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-05-11 21:36:45 -0700

    added additional files to make website modular

---
+++ scripts/portfolioProjects.js
@@ -0,0 +1,16 @@
+var allProjects = [
+  {
+    title: 'About Me Project',
+    projectUrl: 'https://github.com/sendjmoon/about-me',
+    publishedOn: 'April 18, 2016',
+    imageUrl: '<img src="images/stormtrooper-hd-bg.jpg"></img>',
+    body: '<p> Lucas ipsum dolor sit amet carnor tib fode nautolan bail obi-wan darpa bothawui abyssin skywalker. Nute rendar antilles arvel katarn saleucami. Lepi kel orus karrde ansionian. Shistavanen bossk vader quarren veila gwurran dunwell jax. Wol jarael gran r5-d4 anx calrissian. Lahara jamillia mace nar r2-d2 boba senesca. Terrik asajj gank carlist keshiri oola taun skywalker derlin. Bria whiphid amedda kit. Wessell tierce gen\'dai chewbacca rex xappyh leia offee zev. Ahsoka wicket panaka nautolan nute oswaft jax c-3p0. </p>'
+  },
+  {
+    title: 'Cookie Stand Project',
+    projectUrl: 'https://github.com/sendjmoon/cookie-stand-2',
+    publishedOn: 'April 22, 2016',
+    imageUrl: '<img src="images/darth-vader-snow.jpg"></img>',
+    body: '<p> Did you find any droids? No, sir. If there were any on board, they must also have jettisoned. Send a scanning crew on board. I want every part of this ship checked. Yes, sir. I sense something...a presence I haven\'t felt since... Get me a scanning crew in here on the double. I want every part of this ship checked! Boy, it\'s lucky you had these compartments. I use them for smuggling. I never thought I\'d be smuggling myself in them. This is ridiculous. Even if I could take off, I\'d never get past the tractor beam. </p>'
+  }
+];

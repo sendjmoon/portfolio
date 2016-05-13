4baecc8402d2416c8b1492fb7e0c47b4339b3ab7
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-05-12 18:29:13 -0700

    js files renamed for semantic purposes

---
+++ scripts/projects.js
@@ -0,0 +1,33 @@
+var appendProjects = [];
+
+function Project(obj) {
+  this.title = obj.title;
+  this.publishedOn = obj.publishedOn;
+  this.imageUrl = obj.imageUrl;
+  this.projectUrl = obj.projectUrl;
+  this.body = obj.body;
+}
+
+Project.prototype.toHtml = function() {
+  var $newProject = $('article.template').clone();
+
+  $newProject.find('h1').html(this.title);
+  $newProject.find('.project-link').attr('href', this.projectUrl);
+  $newProject.find('.project-link').text('Click me to view the Project!');
+  $newProject.find('.publish-date').html(this.publishedOn);
+  $newProject.find('.article-image').html(this.imageUrl);
+  $newProject.find('.article-body').html(this.body);
+
+  $newProject.removeClass('template');
+  $newProject.addClass('project-article');
+  return $newProject;
+};
+
+//followed same code provided from pair programming in today's lab
+allProjects.forEach(function(obj) {
+  appendProjects.push(new Project(obj));
+});
+
+appendProjects.forEach(function(obj) {
+  $('#articles').append(obj.toHtml());
+});
